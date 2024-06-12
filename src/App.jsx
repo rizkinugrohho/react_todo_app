67adc5216b170e24542d6b04f5eea0aba11db290
A A S <rizkynhg@gmail.com>
2024-06-12 10:43:49 +0700

    Update react-app-todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import './App.css'
+import React, { useState } from 'react'
+import Todos from './components/Todos';  // Lalukan Import
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Have lunch with Guru Domba',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      completed: false,
+    },
+  ])
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      <Todos todos={todos} />
     </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
 }
 
 export default App
