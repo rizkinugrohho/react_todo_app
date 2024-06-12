1bc393c3d1e4a15aee113a00afd6e4a748051b57
A A S <rizkynhg@gmail.com>
2024-06-12 11:30:02 +0700

    Update project Todo 2

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { TodoContext } from '../App';
 
 const TodoItem = ({ todo }) => {
-    return (
-      <div style={styles.todoItem}>
-        <p>{todo.title}</p>
-      </div>
-    )
-  }
-  
-  const styles = {
-    todoItem: {
-      border: '2px solid #f4f4f4',
-      fontSize: '24px',
-    },
-  }
-  
-  export default TodoItem
+  const { toggleCompleted, deleteTodo } = useContext(TodoContext);
+
+  return (
+    <div style={styles.todoItem}>
+      <input
+        type="checkbox"
+        style={styles.checkbox}
+        checked={todo.completed}
+        onChange={() => toggleCompleted(todo.id)}
+      />
+      <p style={getTodoTitleStyle(todo.completed)}>{todo.title}</p>
+      <button style={styles.button} onClick={() => deleteTodo(todo.id)}>
+        x
+      </button>
+    </div>
+  );
+};
+
+const getTodoTitleStyle = (completed) => ({
+  textDecoration: completed ? 'line-through' : 'none',
+});
+
+const styles = {
+  todoItem: {
+    border: '2px solid #f4f4f4',
+    fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0 20px',
+  },
+  checkbox: {
+    height: '18px',
+    width: '18px',
+  },
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
+  },
+};
+
+export default TodoItem;
